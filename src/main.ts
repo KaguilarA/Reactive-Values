a51d73543cf895c35cb46b1454bc69528ee85ef5
Kevin Aguilar <kevin.231@hotmail.com>
2025-08-26 15:10:33 -0600

    add documentation

--- src/main.ts
+++ src/main.ts
@@ -3,44 +3,40 @@ import reactiveValue from './values/reactiveValue';
 import computedValue from './values/computedValue';
 
 const count = reactiveValue(0);
+const doubleCount = computedValue(() => count() * 2, [count]);
 
-const doubleCount = computedValue(() => count.get() * 2, [count]);
-
-function setupCompuedCounter() {
+function setupComputedCounter() {
   const htmlElement = document.querySelector<HTMLSpanElement>('#computedCounter')!;
 
-  const setCounter = () => {
-    htmlElement.innerHTML = `${doubleCount.get()}`;
-  }
-
-  setCounter();
+  doubleCount.effect((val) => {
+    htmlElement.innerHTML = `${val}`;
+  });
 }
 
 function setupReactiveCounter() {
-  const htlmElement = document.querySelector<HTMLButtonElement>('#counter')!;
-  const setCounter = (newCount: number) => {
-    count.set(newCount);
-    htlmElement.innerHTML = `count is ${count.get()}`;
-  }
+  const htmlElement = document.querySelector<HTMLButtonElement>('#counter')!;
 
-  setCounter(count.get());
-  setupCompuedCounter();
-  htlmElement.addEventListener('click', () => {
-    setCounter(count.get() + 1);
-    setupCompuedCounter();
-  })
+  count.effect((val) => {
+    htmlElement.innerHTML = `count is ${val}`;
+  });
+
+  htmlElement.addEventListener('click', () => {
+    count.set(count.get() + 1);
+  });
+
+  setupComputedCounter();
 }
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#example')!.innerHTML = `
   <div>
-    <h1>Reactive Core</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      The computed value is <spam id="computedCounter"></spam>
+      The computed value is <span id="computedCounter"></span>
     </p>
   </div>
-`
+`;
 
 setupReactiveCounter();
+
