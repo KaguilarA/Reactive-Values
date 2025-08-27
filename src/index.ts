884519fd41b0ef2d1d8efc8ac23f465eb2b161b3
Kevin Aguilar <kevin.231@hotmail.com>
2025-08-27 01:22:05 -0600

    add styles to demo and add index export

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Core reactive value
+export { default as reactiveValue } from "./values/reactiveValue";
+
+// Computed values
+export { default as computedValue } from "./values/computedValue";
+
+// Utils
+export { default as deepEqual } from "./utils/deepEqual";
+
+// Types & Interfaces
+export type { ReactiveValue } from "./interface/Reactive";
+export type { ComputedValue } from "./interface/Computed";
